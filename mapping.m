08d7e0a890203cd58cb2db1121cf2fff733eec7a
kazel1990 <kazel1990@gmail.com>
2017-06-06 17:03:00 +0900

    add function mapping

---
+++ mapping.m
@@ -0,0 +1,24 @@
+function [ index ] = mapping( fO, fB )
+% input arguments
+% fO: Nx2 matrix of x,y coordinate of features in Object
+% fB: Mx2 matrix of x,y coordinate of features in Background
+%
+% output arguments
+% index: Nx1 array of indexes of matching points in fB
+%
+% time complexity: O(max(N,M)^2*min(N,M))
+
+N = size(fO,1);
+M = size(fB,1);
+
+mat = zeros(M,N);
+for i = 1:M
+    for j = 1:N
+        mat(i,j) = sum((fO(j,:)-fB(i,:)).^2);
+    end
+end
+
+index = hungarian(mat);
+
+end
+
